62a82d9efcb7667f0999a2b805c237be1dc94709
Chouteau <tetyana.chouteau@passculture.app>
2023-01-18 14:40:23 +0100

    ajout *

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import * as React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./styles.css";
 
+/* reactchallenges.live */
+
+/* Instruction: 
+Implment a catch route for all routes which are not under defined routes scope*/
+
 const Home = () => <h1>Home</h1>;
 const Settings = () => <h1>Settings</h1>;
-const CatchRoute = () => <h1>404</h1>;
+const Other = () => <h1>Les autres</h1>;
 
 export default function App() {
   return (
@@ -16,12 +21,15 @@ export default function App() {
         <li>
           <Link to="/settings">Settings</Link>
         </li>
+        <li>
+          <Link to="/mimporte">M'importe</Link>
+        </li>
       </ul>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/settings" element={<Settings />} />
-        <Route path="*" element={<CatchRoute />} />
+        <Route path="*" element={<Other />} />
       </Routes>
     </Router>
   );
